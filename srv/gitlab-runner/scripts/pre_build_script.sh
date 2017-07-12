35e1e94e4810ff529eb0152fd9b7904fc78ee8af
namutaka <namutaka+g@gmail.com>
2017-07-12 22:20:39 +0900

    Add func setup_permission to pre_build_script

--- srv/gitlab-runner/scripts/pre_build_script.sh
+++ srv/gitlab-runner/scripts/pre_build_script.sh
@@ -1,5 +1,29 @@
 # pre_build_script.sh
 
+
+#
+# Usage:
+#   setup_permission [--access-code] [DEST_DIR]
+#
+function setup_permission() {
+  local requires_access_to_code=false
+  if [ "$1" == "--access-code" ]; then
+    requires_access_to_code=true
+    shift
+  fi
+  local dest_path=$1
+
+  local dest_dir=/srv/nginx/pages/$CI_PROJECT_PATH_SLUG/$dest_path
+  mkdir -p $dest_dir
+
+  cat <<EOS > $dest_dir/.gitlab-info.json
+  {
+    "project_id": $CI_PROJECT_ID,
+    "requires_access_to_code": $requires_access_to_code
+  }
+EOS
+
+}
 function deploy_review() {
   local src_path=$1
 
